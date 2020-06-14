9a557ff02eae4c22fd645ff243e0025e2f33fea5
Otavio Oliveira <otaviojf_oliveira@outlook.com>
2020-06-14 13:04:07 -0300

    Formatting adjustment

--- LiquidGalaxyController/LiquidGalaxyController.cpp
+++ LiquidGalaxyController/LiquidGalaxyController.cpp
@@ -17,56 +17,64 @@ Otávio Jesus França Oliveira - GSoC 2020
 #include "SoftwareSerial.h"
 #include "LiquidGalaxyController.h"
 
-LiquidGalaxyController::JoysticSetup(int piny, int pinx, int button) // JOYSTICK CONTROLLER CONFIGURATION FUNCTION
+LG_JoysticSetup::LG_JoysticSetup(int piny, int pinx, int button,int *_Range) // JOYSTICK CONTROLLER CONFIGURATION FUNCTION
 {
+	if(button)
+	{
+		pinMode(_pinPush,INPUT);
+		_pinPush = button;
+	}
 	_piny = piny;
 	_pinx = pinx;
-	_pinPush = button;
-	pinMode(_pinPush,INPUT);
+	_range = _Range;
+	
 }
 
-char LiquidGalaxyController::joysticRead() // FUNCTION FOR TO READ THE JOYSTIC CONTROLLER
+char LG_JoysticSetup:: JoysticRead() // FUNCTION FOR TO READ THE JOYSTIC CONTROLLER
 {
-    if(analogRead(_piny) > JoysticRanges[0]) return 'B';//IF THE JOYSTIC IS MOVED BACK
-    else if(analogRead(_piny) < JoysticRanges[1]) return 'F';//IF THE JOYSTIC IS MOVED FRONT
+    if(analogRead(_piny) > _range[0]) return 'B';//IF THE JOYSTIC IS MOVED BACK
+    else if(analogRead(_piny) < _range[1]) return 'F';//IF THE JOYSTIC IS MOVED FRONT
 
-    if(analogRead(_pinx) > JoysticRanges[2]) return 'R';//IF THE JOYSTIC IS MOVED RIGTH
-    else if(analogRead(_pinx) < JoysticRanges[3]) return 'L';//IF THE JOYSTIC IS MOVED LEFT
+    if(analogRead(_pinx) > _range[2]) return 'R';//IF THE JOYSTIC IS MOVED RIGTH
+    else if(analogRead(_pinx) < _range[3]) return 'L';//IF THE JOYSTIC IS MOVED LEFT
     
-    if(digitalRead(_pinPush))return 'P'; //IF THE BUTTON IS PRESSED 
+    if(_pinPush != 0)
+    {
+    	if(digitalRead(_pinPush))return 'P'; //IF THE BUTTON IS PRESSED 
+    }
 
     return 0;
 }
 
-LiquidGalaxyController::UltrasonicSetup(int trig, int echo) // PIN MODE SETUP 
+LG_UltrasonicSetup::LG_UltrasonicSetup(int _Trig, int _Echo) // PIN MODE SETUP 
 {
-  	_trig = trig;
-  	_echo = echo;
-  	pinMode(_trig,OUTPUT); // Define the _trig pin OUTPUT
-  	digitalWrite(_trig,LOW);
-  	pinMode(_echo,INPUT);
+  	pinMode(_Trig,OUTPUT); // Define the _trig pin OUTPUT
+  	digitalWrite(_Trig,LOW);
+  	pinMode(_Echo,INPUT);
+  	_trig = _Trig;
+  	_echo = _Echo;
 }
 
-int LiquidGalaxyController::UltrasonicMensure()
+int LG_UltrasonicSetup::UltrasonicMensure()
 {
     digitalWrite(_trig, LOW);  
     delayMicroseconds(2);  
     digitalWrite(_trig, HIGH);  
     delayMicroseconds(10);  
     digitalWrite(_trig, LOW);  
-    long _duration = pulseIn(_echo,HIGH);  
-    long _distance = _duration /29 / 2 ; 
+    _duration = pulseIn(_echo,HIGH);  
+    _distance = _duration /29 / 2 ; 
 	 
     return _distance;
 }
 
-LiquidGalaxyController::KeypadSetup(int *keyMatriz,byte *rowsPins,byte *colunsPins,byte nRow,byte nColun) // This function receive the all information about the keyboard 
+LG_Keypad::LG_Keypad(int *keyMatriz,byte *rowsPins,byte *colunsPins,byte nRow,byte nColun) // This function receive the all information about the keyboard 
 {                                                                                      // And map the pins and variables
   	_rows = nRow;
- 	 _coluns = nColun;
+ 	_coluns = nColun;
   	_colunsPin = colunsPins ;
   	_rowsPin = rowsPins ;
- 	 _keys = keyMatriz;
+ 	_keys = keyMatriz;
   
   	for(int x=0;x < _rows; x++)
     	{
@@ -77,7 +85,7 @@ LiquidGalaxyController::KeypadSetup(int *keyMatriz,byte *rowsPins,byte *colunsPi
     	pinMode(_colunsPin[_y], INPUT);
 }
 
-int LiquidGalaxyController::KeyUpdate()// This function is responsible for to updating the keyboard, if anykey is pressed
+int LG_Keypad::KeyUpdate()// This function is responsible for to updating the keyboard, if anykey is pressed
 {
   	 for(int _x=0; _x < _rows; _x ++) 
   		{
@@ -95,7 +103,7 @@ int LiquidGalaxyController::KeyUpdate()// This function is responsible for to up
     	}  return 0;
 }
 
-int LiquidGalaxyController::KeyPress()
+int LG_Keypad::KeyPress()
 {
 	_a = KeyUpdate();
 	
--- LiquidGalaxyController/LiquidGalaxyController.h
+++ LiquidGalaxyController/LiquidGalaxyController.h
@@ -18,32 +18,35 @@ Otávio Jesus França Oliveira - GSoC 2020
 
 #include "Arduino.h"
 
-class JoysticSetup
+class LG_JoysticSetup
 {
 	public:
-		JoysticSetup(int piny, int pinx, int button);
-		char joysticRead();
+		LG_JoysticSetup(int piny, int pinx, int button,int *_Range);
+		char JoysticRead();
 		
 	private:
 		int _piny,_pinx,_pinPush; // ALL VARIABLES ARE CHANGED
+		byte *_range;
 	
 };
 
-class UltrasonicSetup
+class LG_UltrasonicSetup
 {
 	public:
-		UltrasonicSetup(int trig, int echo);
+		LG_UltrasonicSetup(int _Trig, int _Echo);
 		int  UltrasonicMensure();
 		
 	private:
 		int _trig, _echo;
+		long _duration;
+		long _distance;
 	
 };
 
-class KeypadSetup
+class LG_Keypad
 {
 	public:
-		KeypadSetup(int *keyMatriz,byte *rowsPins,byte *colunsPins,byte nRow,byte nColun);
+		LG_Keypad(int *keyMatriz,byte *rowsPins,byte *colunsPins,byte nRow,byte nColun);
 		int KeyPress();
 		
 	private:
@@ -57,15 +60,38 @@ class KeypadSetup
     	int _b = 0;
 };
 	
-class 
+class LG_VoiceSetup
 {
 	public:
-		int VoiceRead();
+		VoiceSetup(int _Rx,int _Tx, int _pinO);				
 		void RecordCommands(int Ncommand);
 		void StartRecognition(int group);
+		void GetRecognition(); 
 		
 	private:
+		int VoiceRead();
+		void VoiceSetPinOut(int _pin);
+		void PinOut(int Command) 
+		
+		int _rx, _tx;
+		byte _z[27]; // antigo a
+    	int _pinOut; // Voice record indicator
+    	int _Command;
+    	byte a[27];
+    	char Rec[] = {0xaa,0x03,0x20};// Comand test
+    	int gorups[] = {0,1,2,3,4}; // Array with groups index
+    	byte ComandsNumber[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10,0x11,0x12,0x13,0x14};
+    	byte groups[5] [8]={
+      		{0xAA,0x06,0x30,0x01,0x02,0x03,0x04,0x0A}, // PLace Groups this command is used to recognize the voice commands of the first group
+      		{0xAA,0x06,0x30,0x05,0x06,0x07,0x08,0x0A}, // First Group
+      		{0xAA,0x06,0x30,0x09,0x0A,0x0B,0x0C,0x0A}, // Second Group
+      		{0xAA,0x06,0x30,0x0D,0x0E,0x0F,0x10,0x0A}, // Third Group
+      		{0xAA,0x06,0x30,0x11,0x12,0x13,0x14,0x0A} };
 };
+
+#endif
+
+
 //	public:
 		//void JoysticSetup(int piny, int pinx, int button);
 		//char joysticRead();
@@ -81,21 +107,21 @@ class
 		//void StartRecognition(int group);
 
 		
-	private:
+	//private:
 		//int _piny,_pinx,_pinPush; // ALL VARIABLES ARE CHANGED
     	//int _trig, _echo;
    		//byte _rows, _coluns;
     	//byte *_rowsPin;
     	//byte *_colunsPin;
-    	int _rx, _tx; 
+    	//int _rx, _tx; 
     	//int *_keys;
     	//int _a = 0;
     	//int _b = 0;
-    	byte _z[27]; // antigo a
-    	int _pinOut; // Voice record indicator
-    	int _Command;
+    	//byte _z[27]; // antigo a
+    	//int _pinOut; // Voice record indicator
+    	//int _Command;
 
-    	byte a[27];
+    	/*byte a[27];
     	char Rec[] = {0xaa,0x03,0x20};// Comand test
     	int gorups[] = {0,1,2,3,4}; // Array with groups index
     	byte ComandsNumber[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10,0x11,0x12,0x13,0x14};
@@ -104,13 +130,13 @@ class
       		{0xAA,0x06,0x30,0x05,0x06,0x07,0x08,0x0A}, // First Group
       		{0xAA,0x06,0x30,0x09,0x0A,0x0B,0x0C,0x0A}, // Second Group
       		{0xAA,0x06,0x30,0x0D,0x0E,0x0F,0x10,0x0A}, // Third Group
-      		{0xAA,0x06,0x30,0x11,0x12,0x13,0x14,0x0A} }; 
+      		{0xAA,0x06,0x30,0x11,0x12,0x13,0x14,0x0A} }; */
       		
-      	void VoiceSetPinOut(int _pin); //new
-      	void PinOut(int Command); // Verificar necessidade
-      	void VoiceSetPinOut(int _pin);
+      	//void VoiceSetPinOut(int _pin); //new
+      	//void PinOut(int Command); // Verificar necessidade
+       //void VoiceSetPinOut(int _pin);
       			
 		//int KeyUpdate(); // GANGED THE TIPE
 		
 
-#endif
+
