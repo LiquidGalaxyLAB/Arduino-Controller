c0c8002b5dcd06abfd23168ec6128a3edf6956c0
Otavio Oliveira <otaviojf_oliveira@outlook.com>
2020-06-14 16:38:28 -0300

    Created LG_JoystickControll example with library

---
+++ LiquidGalaxyController/examples/LG_JoystickControl/LG_JoystickControl.ino
@@ -0,0 +1,16 @@
+#include <LiquidGalaxyController.h>
+
+int JoysticRanges[] = {3200,100,3200,600};        // You can check these values just by reading the analog inputs corresponding to the joystic 
+                                                  //and define the value that you consider best
+LG_JoysticSetup joystic(A12,A13,4,JoysticRanges); //Insert the pins corresponding to: (Analog Y Axis, Analog X Axis, Push pin, Value_Range)
+                                                  //The range is used to identify from which analog reading variation value the commands will be recognized
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+   char joy = joystic.JoysticRead();// JOYSTICK FUNCTION
+   if(joy) Serial.println(joy);
+}
