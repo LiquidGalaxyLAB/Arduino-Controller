a1aca0dcac1dbe951a0e21f39a20d635fabbfdf7
Otavio Oliveira <otaviojf_oliveira@outlook.com>
2020-07-12 20:34:04 -0300

    Definition of all camera movements

--- Server/SerialServer.py
+++ Server/SerialServer.py
@@ -1,79 +1,110 @@
-import argparse
-import pynput
-import time
 import serial
 
-# from pymouse import PyMouse
-# m = PyMouse()
-
-from pynput.mouse import Button, Controller as MouseController
-
-mouse = MouseController()
 from pynput.keyboard import Key, Controller as KeyboardController
 
 keyb = KeyboardController()
 
-comport = serial.Serial('/dev/ttyUSB0',9600)
-#comport = serial.Serial('COM3', 9600)
+#comport = serial.Serial('/dev/ttyUSB0', 9600)
 
+
+comport = serial.Serial('COM3', 9600)
 
 def Position_Controller(dataRec):
-    data = int(dataRec)
+    data = dataRec
+
     print(data)
-    if data == 1:
-        # mouse.position = ((width / 2), (height / 2))
-        mouse.scroll(0, 1)  # zoom in
-    if data == 2:
-        # mouse.position = ((width / 2), (height / 2))
-        mouse.scroll(0, -1)  # zoom out
-    if data == 3:
+
+    if "linear" in data:
+        keyb.press('r')
+    else:
+        keyb.release('r')
+    if "zOut" in data:
+        keyb.press(Key.page_up)
+    else:
+        keyb.release(Key.page_up)
+    if "zIn" in data:
+        keyb.press(Key.page_down)
+    else:
+        keyb.release(Key.page_down)
+    if "right" in data:
         keyb.press(Key.right)  # RIGHT
     else:
         keyb.release(Key.right)
-    if data == 4:
+    if "left" in data:
         keyb.press(Key.left)  # LEFT
     else:
         keyb.release(Key.left)
-    if data == 5:
+    if "up" in data:
         keyb.press(Key.up)  # UP
     else:
         keyb.release(Key.up)
-    if data == 6:
+    if "down" in data:
         keyb.press(Key.down)  # DOWN
     else:
         keyb.release(Key.down)
-        
-    if data == 7:
+    if "CamUp" in data:
+        keyb.press(Key.ctrl)
+        keyb.press(Key.up)
+    else:
+        keyb.release(Key.ctrl)
+        keyb.release(Key.up)
+    if "CamDown" in data:
+        keyb.press(Key.ctrl)
+        keyb.press(Key.down)
+    else:
+        keyb.release(Key.ctrl)
+        keyb.release(Key.down)
+    if "CamRight" in data:
+        keyb.press(Key.ctrl)
+        keyb.press(Key.right)
+    else:
+        keyb.release(Key.ctrl)
+        keyb.release(Key.right)
+    if "CamLeft" in data:
+        keyb.press(Key.ctrl)
+        keyb.press(Key.left)
+    else:
+        keyb.release(Key.ctrl)
+        keyb.release(Key.left)
+    if "rollRight" in data:
         keyb.press(Key.shift)
         keyb.press(Key.right)
     else:
         keyb.release(Key.shift)
         keyb.release(Key.right)
-    if data == 8:
+    if "rollLeft" in data:
         keyb.press(Key.shift)
         keyb.press(Key.left)
     else:
         keyb.release(Key.shift)
         keyb.release(Key.left)
-    if data == 9:
+    if "tiltUp" in data:
         keyb.press(Key.shift)
         keyb.press(Key.up)  # UP Tilt
     else:
         keyb.release(Key.shift)
         keyb.release(Key.up)
-    if data == 10:
+    if "tiltDown" in data:
         keyb.press(Key.shift)
         keyb.press(Key.down)  # DOWN Tilt
     else:
         keyb.release(Key.shift)
         keyb.release(Key.down)
 
+       # f = open("/tmp/query.txt", "w")
+       # f.write(data)
+       # f.close()
+
+    data = 0
+
 
 if __name__ == "__main__":
-    # width, height = m.screen_size()
+
     while True:
-        valor = comport.readline().decode()
+        valor = comport.readline().decode('UTF-8')
+
         print(valor)
+
         Position_Controller(valor)
 
 # comport.close()
