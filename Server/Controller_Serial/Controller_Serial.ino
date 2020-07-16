0210dfcd8429ae213b6e50530fe424df2b0b82cf
Otavio Oliveira <otaviojf_oliveira@outlook.com>
2020-07-15 23:37:36 -0300

    Add Voice Commands FlyTo

--- Server/Controller_Serial/Controller_Serial.ino
+++ Server/Controller_Serial/Controller_Serial.ino
@@ -1,5 +1,17 @@
 #include <LiquidGalaxyController.h>
-
+#include <SoftwareSerial.h>
+SoftwareSerial VR3(22,23);
+//-------------------------------------------------- Voice Commands
+byte a[27];
+int rec =17;
+int _pinOut = 18;
+int received;
+byte headVoice[4]={0xAA,0x02,0x31,0x0A};
+byte groups[4] [11]={
+      {0xAA,0x09,0x30,0x01,0x02,0x03,0x04,0x12,0x13,0x14,0x0A}, // First Group
+      {0xAA,0x09,0x30,0x05,0x06,0x07,0x08,0x11,0x13,0x14,0x0A}, // Second Group 
+      {0xAA,0x09,0x30,0x09,0x0A,0x0B,0x0C,0x11,0x12,0x14,0x0A},  
+      {0xAA,0x09,0x30,0x0D,0x0E,0x0F,0x10,0x11,0x12,0x13,0x0A}};
 //---------------------------------------------------------------------------------------------
 const byte NumberRows = 4;
 const byte NumberColuns = 4;
@@ -29,12 +41,43 @@ int moviment, moviment2 =0;
 void setup() 
 {
   Serial.begin(9600);
-   
+  VR3.begin(9600);
+  pinMode(_pinOut,OUTPUT); 
+  digitalWrite(_pinOut,LOW);
+  delay(1000);
+  for(int h =0; h<11;h++)VR3.write(groups[0] [h]);
+  delay(100);
+ for(int i = 0; i<11;i++)
+ {
+  a[i] = VR3.read();
+  Serial.print( a[i],HEX);
+  Serial.print("-");
+  delay(1); 
+ }
+ Serial.println("");
+delay(100);
+      VR3.write(0xaa);
+      VR3.write(0x0D);
+      VR3.write(0x0a);
 }
 
 void loop() 
 {   
-  if(digitalRead(5))Tour(15);
+  //if(digitalRead(5))Tour(15);
+    if(digitalRead(5))
+    { while(digitalRead(5) == HIGH){} 
+      grupos(rec);
+      rec ++;
+      if(rec == 21)rec =17;
+    }
+    
+    received = GetRec();
+    if (received <17 && received>0)
+    {
+      Serial.println(received);
+      Serial.println(MakeKML(Coordnates[received-1][0],Coordnates[received-1][1],Coordnates[received-1][2])); 
+    }
+  //--------------------------------------------------
    int key = LGKey.KeyPress(); // KEYBOARD FUNCTION
    if(key){Serial.println(MakeKML(Coordnates[key-1][0],Coordnates[key-1][1],Coordnates[key-1][2]));}
 
@@ -59,7 +102,6 @@ void loop()
               LGMove(0);
               moviment2=0;
             }   
-
 }
 
 //-------------------------------------------------------------------------------------------------------
@@ -91,7 +133,7 @@ String MakeCompleteKML(String longitude, String latitude,String altitude, String
   kml +=altitude ;
   kml += "</altitude><heading>90</heading><tilt>45</tilt><range>";
   kml += range;
-  kml += "</range><gx:altitudeMode>absolute</gx:altitudeMode></LookAt>";*/
+  kml += "</range><gx:altitudeMode>absolute</gx:altitudeMode></LookAt>";
   return kml; 
 }
 //-------------------------------------------------------------------------------------------------------
@@ -166,4 +208,57 @@ void Tour(int Time)
     Serial.println(MakeKML(Coordnates[tour][0],Coordnates[tour][1],Coordnates[tour][2]));
     delay(Time*1000);
   }
+}
+//-------------------------------------------------------------------------------------------------------
+int GetRec()
+{
+    if(VR3.available())
+   {
+      for(int i = 0; i<27;i++)
+     {
+      a[i] = VR3.read();
+      delay(1); 
+     }
+   return a[5];
+  }
+  return 0;
+}
+void grupos(int rec)
+{
+  int h,t;
+  delay(500);
+  switch (rec)
+  {
+    case 17:// First Group
+    //Serial.println("Grupo 1");
+    digitalWrite(_pinOut,!digitalRead(_pinOut)); delay(500);
+    digitalWrite(_pinOut,!digitalRead(_pinOut));
+    for(t =0; t<4;t++)VR3.write(headVoice[t]);
+    delay(500);
+    for(h =0; h<11;h++)VR3.write(groups[0] [h]);
+    break;
+    case 18:// First Group
+    //Serial.println("Grupo 2");
+    for(int p =0;p<4;p++){digitalWrite(_pinOut,!digitalRead(_pinOut)); delay(500);}
+    for(t =0; t<4;t++)VR3.write(headVoice[t]);
+    delay(500);
+    for(h =0; h<11;h++)VR3.write(groups[1] [h]);
+    break;
+    case 19:// First Group
+    //Serial.println("Grupo 3");
+    for(int p =0;p<6;p++){digitalWrite(_pinOut,!digitalRead(_pinOut)); delay(500);}
+    for(t =0; t<4;t++)VR3.write(headVoice[t]);
+    delay(500);
+    for(h =0; h<11;h++)VR3.write(groups[2] [h]);
+    break;
+    case 20:// First Group
+    //Serial.println("Grupo 4");
+    for(int p =0;p<8;p++){digitalWrite(_pinOut,!digitalRead(_pinOut)); delay(500);}
+    for(t =0; t<4;t++)VR3.write(headVoice[t]);
+    delay(500);
+    for(h =0; h<11;h++)VR3.write(groups[3] [h]);
+    break;
+    default:
+    break;
+  } 
 }
